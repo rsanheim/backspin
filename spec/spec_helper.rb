3660c872f58e3be0f06d57d31d7ee3eb66bea494
Rob Sanheim <rsanheim@gmail.com>
2025-06-06 02:13:08 -0500

    move the helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require "bundler/setup"
 require "backspin"
 require "timecop"
 require "tmpdir"
+require_relative "support/backspin_helper"
 
 RSpec.configure do |config|
   config.filter_run_when_matching :focus
@@ -15,27 +16,6 @@ RSpec.configure do |config|
   config.after(:each) do
     # Reset configuration to defaults
     Backspin.reset_configuration!
-  end
-
-  module BackspinHelper
-    # Static timestamp to keep record timestamps consistent
-    def static_time
-      Time.parse("2025-05-01T12:00:00Z")
-    end
-
-    # Setup Backspin to use a temporary directory, and reset config after the block
-    def with_tmp_dir_for_backspin(&block)
-      Dir.mktmpdir("backspin_data") do |dir|
-        path = Pathname.new(dir)
-        Backspin.configure do |config|
-          config.backspin_dir = path
-        end
-
-        block.call
-      ensure
-        Backspin.reset_configuration!
-      end
-    end
   end
 
   config.include(BackspinHelper)
